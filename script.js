4054caeaa119be62d4ffede7b6750fff4cd44057
Lucas-Seles <100317751+Lucas-Seles@users.noreply.github.com>
2022-03-09 18:05:10 -0300

    min-height

--- script.js
+++ script.js
@@ -11,5 +11,4 @@ function limpar(){
 function apagar(){
    var resultado = document.getElementById('resultado').innerHTML;
    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
-
 }
